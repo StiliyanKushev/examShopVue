3a1f239f80c7ad7935e0e4c21677e7814d261b3d
Stiliyan Kushev <stiliyankushev@gmail.com>
2020-04-01 20:37:11 +0300

    fixed a merge error with the client folder

---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="root">
+    <div class="App">
+        <!-- <ToastContainer autoClose={2300} /> -->
+        <AppHeader />
+        <!-- <AppContent /> -->
+        <AppFooter />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppFooter from './components/AppFooter';
+import AppHeader from './components/AppHeader';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+<style>
+/* initial css */
+html {
+  height: 100%;
+}
+
+* {
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+}
+
+body {
+  margin: 0%;
+  font-family: sans-serif;
+}
+</style>
---
+++ client/src/components/AppFooter.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer>
+        <p>Copyright @Softuni exam for VueJS 2020</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "AppFooter",
+}
+</script>
+
+<style>
+/* css for the footer */
+footer {
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-weight: bold;
+
+  background: #0E0A19;
+  color: white;
+}
+footer > p {
+  margin: 0%;
+}
+</style>
